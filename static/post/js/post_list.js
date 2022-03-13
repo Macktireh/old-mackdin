f8e19723746a56f68e22cbe292393afb9d375548
Macktireh <abdimack97@gmail.com>
2022-03-13 16:35:17 +0100

    ajouter un nouveau post et annuler

--- static/post/js/post_list.js
+++ static/post/js/post_list.js
@@ -1,27 +1,62 @@
-const previous_container = document.querySelector(".previous-post-container");
-const previous_img_profile = document.querySelector(
-  ".new-add-post-left-container > img"
-);
-const input_text = document.getElementById("message");
+const previous_container = document.querySelector(".new-add-post2");
+const previous_p_img_container = document.querySelector(".previous-post-img");
 const input_img = document.querySelector(".form-input-file");
+const input_text = document.getElementById("textarea_id");
+const p_text = document.getElementById("previous-message");
+const p_img = document.getElementById("previous-post-image");
+const cancel_add_post = document.getElementById("btn-cancel-add-post");
 
-const path_img = previous_img_profile.src;
 let val;
-
 // console.log(previous_container);
-
-// input_img.addEventListener("change", (e) => {
-//   console.log(e);
-//   console.log(e.target);
-//   console.log(e.target.value);
-// });
 
 input_text.addEventListener("input", (e) => {
   let val = e.target.value;
-  console.log(val);
-  if (val.length >= 1) {
-    previous_container.classList.add("test1");
+  //   console.log(val);
+  if (val.length >= 1 || input_img.value) {
+    previous_container.classList.remove("display-none");
+    previous_container.classList.add("display-block");
+    p_text.textContent = val;
   } else {
-    previous_container.classList.remove("test1");
+    previous_container.classList.remove("display-none");
+    previous_container.classList.remove("display-block");
+    p_text.textContent = val;
   }
+});
+
+input_img.addEventListener("change", function () {
+  const file = this.files[0];
+  //   console.log(file);
+  if (file) {
+    const reader = new FileReader();
+
+    previous_container.classList.remove("display-none");
+    previous_container.classList.add("display-block");
+
+    previous_p_img_container.classList.remove("display-none");
+    previous_p_img_container.classList.add("display-block");
+
+    reader.addEventListener("load", function () {
+      //   console.log(this);
+      //   console.log(this.result);
+      p_img.setAttribute("src", this.result);
+    });
+    reader.readAsDataURL(file);
+  } else {
+    previous_container.classList.remove("display-none");
+    previous_container.classList.remove("display-block");
+
+    previous_p_img_container.classList.remove("display-none");
+    previous_p_img_container.classList.remove("display-block");
+    p_img.setAttribute("src", "");
+  }
+});
+
+cancel_add_post.addEventListener("click", () => {
+  input_text.value = "";
+  input_img.value = "";
+  val = "";
+  p_text.textContent = val;
+  p_img.setAttribute("src", "");
+  previous_p_img_container.classList.add("display-none");
+  previous_container.classList.add("display-none");
 });

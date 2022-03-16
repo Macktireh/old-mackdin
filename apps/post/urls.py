fb7a25c88f9389a8b87d5d591019c962d2cdbc2f
Macktireh <abdimack97@gmail.com>
2022-03-16 22:53:05 +0100

    Modifier et supprimer un post (dans le fichier ajax_add_post.html, j'ai écris le code pour modifier et supprimer un post mais avec ajax et j'ai pas activer pour l'instant cette methode.

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.post_create_list_view, name='post_list'),
-    path('like/', views.like_post, name='like_post'),
     path('add-post/', views.add_post, name='add_post'),
-    # path('<myid>/like/', views.like_post, name='like_post'),
+    path('<int:post_id>/delete-post/', views.delete_post, name='delete_post'),
+    path('<int:post_id>/update-post/', views.update_post, name='update_post'),
+    path('like/', views.like_post, name='like_post'),
 ]
--- apps/post/views.py
+++ apps/post/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import redirect, render
+import os
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 
 from apps.post.models import Like, Post
 from apps.post.forms import PostForm
+
 
 
 @login_required(login_url='sign_in')
 def post_create_list_view(request, *args, **kwargs):
     posts = Post.objects.all()
     user = request.user
-    add_post = False
+    post_form = False
     
     if request.method == 'POST':
         AddPostForm = PostForm(request.POST or None, request.FILES or None)
@@ -19,7 +20,6 @@ def post_create_list_view(request, *args, **kwargs):
             instance = AddPostForm.save(commit=False)
             instance.author = user
             instance.save()
-            add_post = True
             
             return redirect('post_list')
     else:
@@ -30,9 +30,78 @@ def post_create_list_view(request, *args, **kwargs):
         'start_animation': 'feed',
         'posts': posts,
         'AddPostForm': AddPostForm,
-        'add_post': add_post
+        'post_form': post_form
     }
     return render(request, template, context)
+
+
+def update_post(request, post_id):
+    post_edit = get_object_or_404(Post, id=post_id)
+    posts = Post.objects.all()
+    post_form = True  
+    
+    if request.method == 'POST':
+        if len(request.FILES) != 0:
+            if len(post_edit.img) > 0:
+                os.remove(post_edit.img.path)
+            post_edit.img = request.FILES['img']      
+        post_edit.message = request.POST.get('message')
+        post_edit.save()
+        return redirect('post_list')
+        
+    template = 'post/post_list.html'
+    context = {
+        'start_animation': 'feed',
+        'posts': posts,
+        'post_edit': post_edit,
+        'post_form': post_form
+    }
+    return render(request, template, context)
+
+
+def add_post(request):
+    user = request.user
+    AddPostForm = PostForm(request.POST or None, request.FILES or None)
+    if request.is_ajax():
+        if AddPostForm.is_valid():
+            instance = AddPostForm.save(commit=False)
+            instance.author = user
+            instance.save()
+            post = Post.objects.values()
+            return JsonResponse({'status': 'success', 'post_data': list(post)})
+        else:
+            return JsonResponse({'status': 'error'})
+   
+        
+def delete_post(request, post_id):
+    user = request.user
+    # if request.method == 'POST':
+    #     p_id = request.POST.get('post_id')
+    #     instance = Post.objects.get(pk=p_id)
+    #     if user == instance.author:
+    #         instance.delete()
+    #         return JsonResponse({'status': 'Post supprimer'})
+    # else:
+    #     return JsonResponse({'status': 'error'})
+    instance = get_object_or_404(Post, id=post_id)
+    if user == instance.author:
+        instance.delete()
+    return redirect('post_list')
+    
+    
+def list_posts(request):
+    qs = Post.objects.all()
+    data = []
+    for obj in qs:
+        item = {
+            'id': obj.id,
+            'author': obj.author,
+            'message': obj.message,
+            'image': obj.img,
+            'date_created': obj.created
+        }
+        
+        
 
 def like_post(request):
     user = request.user
@@ -61,19 +130,3 @@ def like_post(request):
         }
         return JsonResponse(data, safe=False)
     return redirect('post_list')
-
-
-
-def add_post(request):
-    user = request.user
-    AddPostForm = PostForm(request.POST or None, request.FILES or None)
-    # data = {}
-    if request.is_ajax():
-        if AddPostForm.is_valid():
-            instance = AddPostForm.save(commit=False)
-            instance.author = user
-            instance.save()
-            # data['status']
-            return JsonResponse({'status': 'success'})
-        else:
-            return JsonResponse({'status': 'error'})

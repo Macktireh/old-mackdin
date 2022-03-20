452fc95f03f51f7b6ce7783c538ca379a1983871
Macktireh <abdimack97@gmail.com>
2022-03-20 22:48:54 +0100

    queque ajustement de style des commentaires

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 class PostForm(forms.ModelForm):
     """Form definition for Post."""
-    message = forms.CharField(required=True, widget=forms.Textarea(
+    message = forms.CharField(required=False, widget=forms.Textarea(
         attrs={'cols':'65', 'rows':'2', 'id':'textarea_id', 'placeholder':'Ajouter un post'}))
     img = forms.ImageField(required=False, widget=forms.FileInput(
         attrs={'id':'file', 'class':'form-input-file'}))

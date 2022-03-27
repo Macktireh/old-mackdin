caa8ff3600c41b5d481cd139aaf5dadddad8e83c
Macktireh <abdimack97@gmail.com>
2022-03-27 23:26:29 +0200

    Separation du code comments et post. possibilité consulter les profile des autres utilisateur en cliquant sur leurs image ou nom.

--- apps/comments/urls.py
+++ apps/comments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.comments import views
 
 urlpatterns = [
-    path('api-data/', views.comment_all_data, name='api-comment'),
-    # path('add-comment/', views.add_comment, name='add-comment'),
+    # path('api-data/', views.comment_all_data, name='api-comment'),
+    path('add-comment/', views.add_comment, name='add-comment'),
 ]

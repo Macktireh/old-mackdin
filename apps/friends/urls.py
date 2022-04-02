6fa5156d75292732cfe452aae0488ddad143e211
Macktireh <abdimack97@gmail.com>
2022-04-02 05:54:45 +0200

    gestion des erreurs pour l'iffichage des images bg et profile.

--- apps/friends/urls.py
+++ apps/friends/urls.py
@@ -3,7 +3,6 @@ from apps.friends import views
 
 
 urlpatterns = [
-    # path('', views.list_profiles_view, name='list_all_profiles'),
     path('', views.invites_list_profiles_view, name='my_network'),
     path('connections/', views.my_friends_invites_profiles_view, name='my_friends'),
     path('invitation-received/', views.invites_received_view, name='invitation_received'),

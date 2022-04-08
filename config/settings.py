cd97a4a1670f260318a23f931c9b57e22ca90d44
Macktireh <abdimack97@gmail.com>
2022-04-09 01:52:00 +0200

    delete django_extensions

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ LOCAL_APPS = [
     'apps.friends',
 ]
 
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

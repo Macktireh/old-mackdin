1751d7ac832051a435c010ddb0005efd8a55e63e
Macktireh <abdimack97@gmail.com>
2022-04-15 16:51:39 +0200

    new api news

--- static/post/js/api-news.js
+++ static/post/js/api-news.js
@@ -1,19 +1,37 @@
-const aside = document.querySelector(".container-aside-1")
+const aside = document.querySelector(".container-aside-1");
 
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Host': 'free-news.p.rapidapi.com',
-    'X-RapidAPI-Key': '7dd7c1ff69msh8d88e46f019579fp1a47cdjsn9fc979eccfb3'
-  }
+    "X-RapidAPI-Host": "free-news.p.rapidapi.com",
+    "X-RapidAPI-Key": "7dd7c1ff69msh8d88e46f019579fp1a47cdjsn9fc979eccfb3",
+  },
 };
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Host": "google-news.p.rapidapi.com",
+//     "X-RapidAPI-Key": "7dd7c1ff69msh8d88e46f019579fp1a47cdjsn9fc979eccfb3",
+//   },
+// };
 
-fetch('https://free-news.p.rapidapi.com/v1/search?q=Elon%20Musk&lang=fr&page_size=10', options)
-  .then(response => response.json())
-  .then(response => response.articles)
-  .then(data => {
+// fetch(
+//   "https://google-news.p.rapidapi.com/v1/source_search?source=nytimes.com&lang=en&country=US",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+fetch(
+  "https://free-news.p.rapidapi.com/v1/search?q=Microsoft&lang=fr&page_size=10",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => response.articles)
+  .then((data) => {
     // console.log(data)
-    data.forEach(article => {
+    data.forEach((article) => {
       aside.innerHTML += `
       <a href="${article.link}" target="_blank">
         <li class="box-actu">
@@ -21,51 +39,50 @@ fetch('https://free-news.p.rapidapi.com/v1/search?q=Elon%20Musk&lang=fr&page_siz
           <small>il y a ${timeSince(article.published_date)}</small>
         </li>
       </a>
-      `
-    })
+      `;
+    });
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
+var timeSince = function (date) {
+  if (typeof date !== "object") {
+    date = new Date(date);
+  }
 
-  var timeSince = function(date) {
-    if (typeof date !== 'object') {
-      date = new Date(date);
-    }
-  
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var intervalType;
-  
-    var interval = Math.floor(seconds / 31536000);
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var intervalType;
+
+  var interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    intervalType = "année";
+  } else {
+    interval = Math.floor(seconds / 2592000);
     if (interval >= 1) {
-      intervalType = 'année';
+      intervalType = "mois";
     } else {
-      interval = Math.floor(seconds / 2592000);
+      interval = Math.floor(seconds / 86400);
       if (interval >= 1) {
-        intervalType = 'mois';
+        intervalType = "jour";
       } else {
-        interval = Math.floor(seconds / 86400);
+        interval = Math.floor(seconds / 3600);
         if (interval >= 1) {
-          intervalType = 'jour';
+          intervalType = "heure";
         } else {
-          interval = Math.floor(seconds / 3600);
+          interval = Math.floor(seconds / 60);
           if (interval >= 1) {
-            intervalType = "heure";
+            intervalType = "minute";
           } else {
-            interval = Math.floor(seconds / 60);
-            if (interval >= 1) {
-              intervalType = "minute";
-            } else {
-              interval = seconds;
-              intervalType = "seconde";
-            }
+            interval = seconds;
+            intervalType = "seconde";
           }
         }
       }
     }
-  
-    if (interval > 1 || interval === 0) {
-      intervalType += 's';
-    }
-  
-    return interval + ' ' + intervalType;
-  };
+  }
+
+  if (interval > 1 || interval === 0) {
+    intervalType += "s";
+  }
+
+  return interval + " " + intervalType;
+};
